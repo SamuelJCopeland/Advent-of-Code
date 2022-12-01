3ed2ef2fdb280ab3a1d5b6a4099ebc48c9656c75
SamuelJCopeland <73198633+SamuelJCopeland@users.noreply.github.com>
2022-12-01 10:21:59 -0500

    Advent of Code 2022 Day 1

---
+++ 2022/day1-1.py
@@ -0,0 +1,14 @@
+elves = []
+cals = 0
+with open(r"2022/input1.txt", "r") as input_file:
+    for line in input_file:
+        #print(line)
+        if line == "\n":
+            elves.append(cals)
+            #print(f"cals: {cals}")
+            cals = 0
+        else:
+            cals += int(line)
+elves.append(cals) 
+elves.sort(reverse=True)
+print(elves[0])
